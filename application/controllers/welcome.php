28100f20a51b16e602370a509203ebd1fce10201
Ornind <pimjangka@hotmail.com>
2015-02-22 18:01:26 +0700

    Create CSS, JS, PICS, getfeed()

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -21,6 +21,20 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+	public function getfeed ($fbID){
+        
+        $url = "https://graph.facebook.com/".$fbID."/feed?limit=100".'&access_token=1566772446943313|fd4d1e9a8b3ced39e57b3fe7b43b03cd';
+     	$requests = file_get_contents($url);
+       	$fb_response = json_decode($requests);
+
+		echo "<br>";
+    	foreach ($fb_response->data as $item) {
+    		if($item->from->name != 'TrueYou'){
+    			echo "->message : ".$item->message."<br> ".$item->from->name."<br><br>";
+    		}
+    	}
+    
+	}
 }
 
 /* End of file welcome.php */
